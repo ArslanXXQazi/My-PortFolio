04c04d5ac43e0445197dc8e23828c2d077e86243
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-07 09:46:59 -0700

    jkabsc

---
+++ lib/src/controller/components/custom_text.dart
@@ -0,0 +1,77 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WhiteNormalText extends StatelessWidget {
+  final String? text;
+  final VoidCallback? onTap;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? textColor;
+  final TextAlign? textAlign; // New parameter
+
+  const WhiteNormalText({
+    super.key,
+    this.text,
+    this.onTap,
+    this.fontSize,
+    this.fontWeight,
+    this.textColor,
+    this.textAlign, // Added to constructor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap ?? null,
+      child: Text(
+        text ?? "",
+        textAlign: textAlign ??
+            TextAlign.center, // Default to center, override if provided
+        style: GoogleFonts.poppins(
+          fontSize: fontSize ?? 16,
+          fontWeight: fontWeight ?? FontWeight.w500,
+          color: textColor ?? Colors.black,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class SegoeText extends StatelessWidget {
+  final String? text;
+  final VoidCallback? onTap;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? textColor;
+  final TextAlign? textAlign; // New parameter
+
+  const SegoeText({
+    super.key,
+    this.text,
+    this.onTap,
+    this.fontSize,
+    this.fontWeight,
+    this.textColor,
+    this.textAlign, // Added to constructor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap ?? null,
+      child: Text(
+        text ?? "",
+        textAlign: textAlign ??
+            TextAlign.center, // Default to center, override if provided
+        style: TextStyle(
+          fontSize: fontSize ?? 14,
+          fontWeight: fontWeight ?? FontWeight.w600,
+          color: textColor ?? Colors.black,
+        ),
+      ),
+    );
+  }
+}
--- lib/src/view/desktop_views/desktop_view.dart
+++ lib/src/view/desktop_views/desktop_view.dart
@@ -1,19 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/src/controller/components/custom_text.dart';
 
 class DesktopView extends StatelessWidget {
   const DesktopView({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(children: [
         Expanded(
-          flex: 20,
-            child: Container(color: Colors.red,)),
+          flex: 10,
+            child: Container(
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Icon(Icons.home),
+                  SizedBox(width: width*.01,),
+                  WhiteNormalText(
+                    onTap: (){},
+                    text: "Home",
+                  )
+                ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(width: width*.01,),
+                    WhiteNormalText(
+                      onTap: (){},
+                      text: "About",
+                    )
+                  ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.abc_outlined),
+                    SizedBox(width: width*.01,),
+                    WhiteNormalText(
+                      onTap: (){},
+                      text: "Services",
+                    )
+                  ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(width: width*.01,),
+                    WhiteNormalText(
+                      onTap: (){},
+                      text: "Portfolio",
+                    )
+                  ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.contact_mail),
+                    SizedBox(width: width*.01,),
+                    WhiteNormalText(
+                      onTap: (){},
+                      text: "Contact",
+                    )
+                  ],),
+
+              ],),
+            )),
         Expanded(
-          flex: 80,
-            child: Container(color: Colors.green,)),
+          flex: 90,
+            child: Container(
+              color: Colors.pinkAccent,
+              child: Row(children: [
+                Expanded(
+                  flex: 65,
+                    child: Container(
+                  color: Colors.red,
+                      child: Column(children: [
+                        
+                      ],),
+                )),
+                Expanded(
+                  flex: 35,
+                    child: Container(
+                )),
+              ],),
+            )),
       ],),
     );
   }
